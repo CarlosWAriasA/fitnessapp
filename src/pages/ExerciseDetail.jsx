aa5f87a9abdab8fdc57be418e02c76882e374e2f
Carlos Arias <carlos.ariasalmanzar@gmail.com>
2024-10-13 00:09:13 -0400

    Adding initial structure

---
+++ src/pages/ExerciseDetail.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ExerciseDetail(props) {
+  return <div></div>;
+}
+
+export default ExerciseDetail;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Home = (props) => <div>Hola</div>;
+
+export default Home;
